e35ac51be51bbd6fa4aeda522eab59796b1a5cf0
Alejandromo125 <alejandromartinortega@gmail.com>
2021-12-06 17:00:46 +0100

    Added archives to add enemies
    Need to add the animations, physics and pathfinding logic for movement.
    Check the A star logic for addition and afterwards we should test if it works correctly

---
+++ Game/Source/Flying_Enemy.h
@@ -0,0 +1,50 @@
+#ifndef __ENEMY_SOLDIER_H__
+#define __ENEMY_SOLDIER_H__
+
+#include "Enemy.h"
+#include "Path.h"
+
+class Enemy_Soldier : public Enemy
+{
+public:
+	// Constructor (x y coordinates in the world)
+	// Creates animation and movement data and the collider
+	Enemy_Soldier(int x, int y);
+
+	// The enemy is going to follow the different steps in the path
+	// Position will be updated depending on the speed defined at each step
+	void Update() override;
+
+	
+	int counter;
+	int explosionDelay;
+	
+
+private:
+	// A set of steps that define the position in the screen
+	// And an animation for each step
+	Path path;
+
+	Animation walkUp;
+	Animation walkDown;
+	Animation walkLeft;
+	Animation walkRight;
+	Animation walkUpLeft;
+	Animation walkUpRight;
+	Animation walkDownLeft;
+	Animation walkDownRight;
+
+	Animation shootUp;
+	Animation shootDown;
+	Animation shootLeft;
+	Animation shootRight;
+	Animation shootUpLeft;
+	Animation shootUpRight;
+	Animation shootDownLeft;
+	Animation shootDownRight;
+
+
+	
+};
+
+#endif // __ENEMY_BROWNSHIP_H__
